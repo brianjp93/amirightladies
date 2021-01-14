8dbd63f6d0f9c36fb91e59674069abb12288c843
Brian Perrett <bperrett@fusionbox.com>
2021-01-14 10:45:39 -0700

    am i right ladies

--- main.py
+++ main.py
@@ -2,24 +2,14 @@ import discord
 from dotenv import load_dotenv
 import os
 import random
+from resources import amirightladies as ladies
 load_dotenv()
 
 
 TOKEN = os.getenv('TOKEN')
 INVITE_LINK = f'https://discord.com/oauth2/authorize?client_id={os.getenv("CLIENT_ID", "")}&scope=bot&permissions=355328'
 print(INVITE_LINK)
-MESSAGE_LIST = [
-    'AM I RIGHT LADIES?',
-    '✩  🎀  𝒶𝓂 𝐼 𝓇𝒾𝑔𝒽𝓉 𝓁𝒶𝒹𝒾𝑒𝓈¿  🎀  ✩',
-    '🐔  🎀  𝒶𝓂 𝐼 𝓇𝒾𝑔𝒽𝓉 𝓁𝒶𝒹𝒾𝑒𝓈¿  🎀  🐔',
-    '¸,ø¤º°`°º¤ø,¸¸,ø¤º°   🎀  𝒶𝓂 𝐼 𝓇𝒾𝑔𝒽𝓉 𝓁𝒶𝒹𝒾𝑒𝓈?  🎀   °º¤ø,¸¸,ø¤º°`°º¤ø,¸',
-    'AM I?',
-    '꧁༺ 𝓪𝓶 𝓲 𝓻𝓲𝓰𝓱𝓽 𝓵𝓪𝓭𝓲𝓮𝓼? ༻꧂',
-    '▞▞▞▞▞▖🅰🅼 🅸 🆁🅸🅶🅷🆃 🅻🅰🅳🅸🅴🆂?▝▞▞▞▞▞',
-    '(¯`’•.¸❤♫♪♥(◠‿◠)♥♫♪❤¸.•’´¯)am i right ladies?(¯`’•.¸❤♫♪♥(◠‿◠)♥♫♪❤¸.•’´¯)',
-    '(🌘‿🌘) am i right ladies? (🌘‿🌘)',
-    '(◉‿◉) am i right ladies? (◉‿◉)',
-]
+
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -32,7 +22,7 @@ class Client(discord.Client):
                 await self.write_cool_message(message.channel)
 
     async def write_cool_message(self, channel):
-        await channel.send(random.choice(MESSAGE_LIST))
+        await channel.send(random.choice(ladies.MESSAGE_LIST))
 
 
 client = Client()
