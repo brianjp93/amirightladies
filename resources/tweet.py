72930e99bb10cbf1fdd6c9a8c163e17e7b1415d0
Brian Perrett <bperrett@fusionbox.com>
2021-01-15 15:06:15 -0700

    am i right ladies

--- resources/tweet.py
+++ resources/tweet.py
@@ -16,13 +16,18 @@ api = twitter.Api(
 )
 
 def get_avatar_tweet_url():
-    day = get_recent_day()
-    qs = f'q=avatar last airbender&count=100&until={day} -RT'.replace(' ', '%20')
-    query = api.GetSearch(raw_query=qs)
-    tweet = random.choice(query)
-    username = tweet.user.screen_name
-    tweet_id = tweet.id
-    return f'https://twitter.com/{username}/status/{tweet_id}'
+    retries = 3
+    retry = 0
+    while retry < retries:
+        day = get_recent_day()
+        qs = f'q=avatar last airbender&count=100&until={day} -RT'.replace(' ', '%20')
+        query = api.GetSearch(raw_query=qs)
+        if query:
+            tweet = random.choice(query)
+            username = tweet.user.screen_name
+            tweet_id = tweet.id
+            return f'https://twitter.com/{username}/status/{tweet_id}'
+        retry += 1
 
 def get_recent_day(days=7):
     """Get any day within the last 7 days
