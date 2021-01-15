6bcafbcbb0f0b0db6815f181eb2f7d1325fe8678
= <bperrett@fusionbox.com>
2021-01-14 20:19:19 -0700

    am i right ladies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import tasks
 from dotenv import load_dotenv
 import os
 import random
@@ -13,6 +14,14 @@ print(INVITE_LINK)
 
 
 class Client(discord.Client):
+    def __init__(self, *args, **kwargs):
+        self.avatar_channel = None
+        super().__init__(*args, **kwargs)
+        self.set_up()
+
+    def set_up(self):
+        self.send_avatar_message.start()
+
     async def on_ready(self):
         print(f'Logged on as {self.user}')
 
@@ -24,8 +33,17 @@ class Client(discord.Client):
                 await self.write_cool_message(message.channel)
 
             if channel_name == 'watch-avatar-lisa':
+                self.avatar_channel = message.channel.id
                 url = tweet.get_avatar_tweet_url()
                 await message.channel.send(url)
+
+    @tasks.loop(hours=1)
+    async def send_avatar_message(self):
+        if self.avatar_channel:
+            channel = self.get_channel(self.avatar_channel)
+            url = tweet.get_avatar_tweet_url()
+            message = f'Hello Lisa, this is your hourly reminder to watch avatar.\n{url}'
+            await channel.send(message)
 
 
     async def write_cool_message(self, channel):
